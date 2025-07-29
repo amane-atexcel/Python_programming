6759a9ad295e43371b78a47310711e503c9debbc
amane-atexcel <amanuelefrem77@gmail.com>
2025-07-29 12:12:58 +0300

    from LMS

---
+++ test.py
@@ -0,0 +1,24 @@
+#Reading of LMS
+#there are six data types listed in the LMS
+num1 = 55
+num2 = 5.3
+print(num1)
+print(num2)
+
+#Lists in Python - ordered collection of similar or different data types
+languages = ["Python", "Dart", "Web", 23]
+print(languages)
+print(languages[1]) #accessing list element
+
+#Tuples
+products = ('XBox', 499.99, "Habibi", 23)
+print(products)
+print(products[2]) #accessing tuple
+
+#sets an unordered collection of unique items
+student_ids = {112, 114, 117, 113}
+print(student_ids)
+
+#Python dictionary >>> ordered collection of items
+capital_city = {"Kenya": "Nairobi", "Nigeria": "Lagos"}
+print(capital_city)
